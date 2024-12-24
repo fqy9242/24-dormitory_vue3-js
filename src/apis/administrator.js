5947028c4c9f00c2552561a2689921ba992d6bb6
qht <qht20050506@outlook.com>
2024-12-24 17:19:04 +0800

    分配宿舍页布局构建优化

--- src/apis/administrator.js
+++ src/apis/administrator.js
@@ -21,3 +21,45 @@ export const getCollegeListApi = () => {
       url: "/administrator/listCollegeName",
     })
 }
+
+
+// 根据班级查询已分配的宿舍
+export const listPlanDormitoryByClassNameApi =  (className)=> {
+  return request({
+    url: "/administrator/listPlanDormitoryByClassName",
+    params: {
+      className
+    }
+  })
+}
+
+export const listDormitoryAreaApi =  () => {
+  return request({
+    url: "/administrator/listDormitoryArea",
+  })
+}
+
+// 根据楼栋信息查询楼栋
+export const listBuildingByBuildingApi = (data) => {
+  return request({
+    url: "/administrator/listBuildingByBuilding",
+    method: "post",
+    data,
+  });
+}
+// 根据楼栋id获取宿舍
+export const listDormitoryByBuildingIdApi = (buildingId) => {
+  return request({
+    url: "/administrator/listDormitoryByBuilding",
+    params: {
+      buildingId
+    }
+  })
+}
+
+// 根据宿舍id获取宿舍详情
+export const getDormitoryDetailApi = (dormitoryId) => {
+  return request({
+    url: `/administrator/getDormitoryById/${dormitoryId}`,
+  })
+}
--- src/views/Admin/PlanDormitory/index.vue
+++ src/views/Admin/PlanDormitory/index.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { getClassListApi, getCollegeListApi } from '@/apis/administrator'
+import { getClassListApi, getCollegeListApi, listPlanDormitoryByClassNameApi, listDormitoryAreaApi,
+    listBuildingByBuildingApi, listDormitoryByBuildingIdApi, getDormitoryDetailApi
+ } from '@/apis/administrator'
 const classData = ref([])
 const total = ref(0)
+const dormitoryList = ref([])
 const collegeList = ref([])
+const buildingList = ref([])
+const thisDormitory = ref({})
 const getClassListData = ref({
     page: 1,
     pageSize: 10,
@@ -17,15 +22,59 @@ const getClassListData = ref({
     girlAmountEnd: null,
     teacherPhoneNumber: '',
     isPlanComplete: null
-
 })
+// 现在正在分配宿舍的班级数据
+const planDormitoryClass = ref({})
+// 选中的班级已分配的宿舍
+const selectClassPlanDormitory = ref([])
+// 区域列表
+const areaList = ref([])
+
+const listPlanDormitoryByClassName = async (className) => {
+    const res = await listPlanDormitoryByClassNameApi(className)
+    selectClassPlanDormitory.value = res.data
+}
+// 获取区域列表
+const listDormitoryArea = async () => {
+    const res = await listDormitoryAreaApi()
+    areaList.value = res.data
+}
+// 获取楼栋列表
+const listDormitoryBuildingByArea = async (areaId) => {
+    const res = await listBuildingByBuildingApi({
+        areaId: areaId,
+        type: planDormitoryClass.value.dormitoryType
+    })
+    buildingList.value = res.data
+}
+// 获取宿舍列表 
+const listDormitoryByBuildingId = async (buildingId) => {
+    const res = await listDormitoryByBuildingIdApi(buildingId)
+    dormitoryList.value = res.data
+}
+    
 const getClassList = async () => {
     const res = await getClassListApi(getClassListData.value)
     classData.value = res.data.records
     total.value = res.data.total
 }
-
-
+// 区域选择变化
+const selectAreaOnChange = (areaName) => {
+    planDormitoryClass.value.buildingId = null
+    listDormitoryBuildingByArea(areaName)
+}
+// 获取宿舍详情
+const getDormitoryDetail = async (dormitoryId) => {
+    const res = await getDormitoryDetailApi(dormitoryId)
+    thisDormitory.value = res.data
+}
+// 宿舍选择变化
+const planDormitoryOnChange = () => {
+    if (planDormitoryClass.value.dormitoryId == null) {
+        return
+    }
+    getDormitoryDetail(planDormitoryClass.value.dormitoryId)
+}
 // 处理分页变化
 const handleCurrentChange = (newPage) => {
     getClassListData.value.page = newPage;
@@ -39,10 +88,28 @@ const handleSizeChange = (newSize) => {
 const queryOnHandle = () => {
     getClassList()
 }
+
 // 获取学院列表
 const getCollegeList = async () => {
     const res = await getCollegeListApi()
     collegeList.value = res.data
+}
+const planDormitoryTypeOnChange = () => {
+    planDormitoryClass.value.areaId = null
+    planDormitoryClass.value.buildingId = null
+    planDormitoryClass.value.dormitoryId = null
+}
+const planDormitoryDialogVisible = ref(false)
+// 表格行点击事件
+const handleRowClick = (row) => {
+    planDormitoryClass.value = row
+    listDormitoryArea()
+    listPlanDormitoryByClassName(row.className)
+    planDormitoryDialogVisible.value = true
+}
+const selectBuildingOnChange = (buildingId) => {
+    planDormitoryClass.value.dormitoryId = null
+    listDormitoryByBuildingId(buildingId)
 }
 onMounted(() => {
     getClassList()
@@ -57,17 +124,105 @@ onMounted(() => {
             </span>
         </div>
         <div class="content">
+
+            <!-- 手动分配宿舍对话框 -->
+            <div class="plan_dormitory_dialog">
+                <el-dialog v-model="planDormitoryDialogVisible" title="分配宿舍" :close-on-click-modal="false">
+                    <div class="class_info_and_plan">
+                        <div class="class_info">
+                            <p>班级名称:{{ planDormitoryClass.className }}</p>
+                            <p>男生人数:{{ planDormitoryClass.boyAmount }}
+                                <span>
+                                    (待分配 {{ planDormitoryClass.boyAmount -
+                                    planDormitoryClass.boyAlreadyPlanDormitoryAmount
+                                    }}人)
+                                </span>
+                            </p>
+                            <p>
+                                女生人数:{{ planDormitoryClass.girlAmount }}
+                                <span>
+                                    (待分配{{ planDormitoryClass.girlAmount -
+                                    planDormitoryClass.girlAlreadyPlanDormitoryAmount
+                                    }}人)
+                                </span>
+                            </p>
+                            <el-tag>
+                                <span>该班级已分配宿舍情况如下表格</span>
+                            </el-tag>
+                        </div>
+                        <div class="plan_form">
+                            <el-form>
+                                <el-form-item label="宿舍类型">
+                                    <el-radio-group v-model="planDormitoryClass.dormitoryType"
+                                        @change="planDormitoryTypeOnChange">
+                                        <el-radio label="1">男生</el-radio>
+                                        <el-radio label="2">女生</el-radio>
+                                    </el-radio-group>
+                                </el-form-item>
+                                <el-form-item label="区域">
+                                    <el-select v-model="planDormitoryClass.areaId"
+                                        :placeholder="!planDormitoryClass.dormitoryType? '请先选择宿舍类型':'请选择宿舍区域'" clearable
+                                        @change="selectAreaOnChange(planDormitoryClass.areaId)"
+                                        :disabled="!planDormitoryClass.dormitoryType">
+                                        <el-option v-for="item in areaList" :key="item.id" :label="item.areaName"
+                                            :value="item.id">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                                <el-form-item label="楼栋">
+                                    <el-select v-model="planDormitoryClass.buildingId"
+                                        :placeholder="planDormitoryClass.areaId? '请选择楼栋':'请先选择区域'" clearable
+                                        :disabled="!planDormitoryClass.areaId"
+                                        @change="selectBuildingOnChange(planDormitoryClass.buildingId)">
+                                        <el-option v-for="item in buildingList" :key="item.id"
+                                            :label="item.buildingName" :value="item.id">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                                <el-form-item label="宿舍">
+                                    <el-select v-model="planDormitoryClass.dormitoryId" clearable
+                                        :placeholder="planDormitoryClass.buildingId? '请选择宿舍':'请先选择楼栋'"
+                                        :disabled="!planDormitoryClass.buildingId" @change="planDormitoryOnChange()">
+                                        <el-option v-for="item in dormitoryList" :key="item.id"
+                                            :label="item.dormitoryNumber" :value="item.id">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                                <span v-show="planDormitoryClass.dormitoryId">
+                                    该宿舍总床位数:{{ thisDormitory.bedAmount }} 可分配 {{ thisDormitory.bedAmount -
+                                    thisDormitory.totalPlanNumber }}人
+                                </span>
+                                <el-form-item label="名额">
+                                    <el-input v-model="planDormitoryClass.planNumber" placeholder="请输入名额" clearable />
+                                </el-form-item>
+                                <el-button>添加</el-button>
+                            </el-form>
+
+                        </div>
+                    </div>
+
+
+                    <el-table :data="selectClassPlanDormitory" style="width: 100%">
+                        <el-table-column label="宿舍类型">
+                            <template #default="{ row }">
+                                {{ row.dormitoryType == 1 ? '男生' : '女生' }}
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="areaName" label="区域" />
+                        <el-table-column prop="buildingName" label="楼栋" />
+                        <el-table-column prop="dormitoryNumber" label="宿舍" />
+                        <el-table-column prop="planNumber" label="名额" />
+                    </el-table>
+
+                </el-dialog>
+            </div>
             <div class="screen">
                 <div class="option">
                     <div class="option_form">
                         <div class="form_item">
                             <span>学院名称</span>
                             <el-select v-model="getClassListData.collegeName" placeholder="请选择学院名称" clearable>
-                                <el-option
-                                    v-for="item in collegeList"
-                                    :key="item"
-                                    :label="item"
-                                    :value="item">
+                                <el-option v-for="item in collegeList" :key="item" :label="item" :value="item">
                                 </el-option>
                             </el-select>
                         </div>
@@ -77,7 +232,8 @@ onMounted(() => {
                         </div>
                         <div class="form_item">
                             <span>年级</span>
-                            <el-input v-model="getClassListData.grade" style="width: 50%;" placeholder="例如2023" clearable />
+                            <el-input v-model="getClassListData.grade" style="width: 50%;" placeholder="例如2023"
+                                clearable />
                         </div>
                         <div class="form_item">
                             <span>男生人数</span>
@@ -94,8 +250,8 @@ onMounted(() => {
                         <div class="form_item">
                             <span>分配状态</span>
                             <el-select v-model="getClassListData.isPlanComplete" placeholder="请选择分配状态" clearable>
-                                <el-option label="已分配完毕" value = "1"/>
-                                <el-option label="未分配完毕" value = "0"/>
+                                <el-option label="已分配完毕" value="1" />
+                                <el-option label="未分配完毕" value="0" />
                             </el-select>
                         </div>
                     </div>
@@ -106,7 +262,7 @@ onMounted(() => {
                 </div>
             </div>
             <div class="class_table">
-                <el-table :data="classData" style="width: 100%">
+                <el-table :data="classData" style="width: 100%" @row-click="handleRowClick">
                     <!-- <el-table-column type="selection" :selectable="selectable" width="55" /> -->
                     <el-table-column prop="collegeName" label="学院名称" />
                     <el-table-column prop="className" label="班级名称" />
@@ -122,9 +278,10 @@ onMounted(() => {
                             <span>{{ row.girlAmount - row.girlAlreadyPlanDormitoryAmount }}</span>
                         </template>
                     </el-table-column>
-                    <el-table-column  label="是否已全部分配">
+                    <el-table-column label="是否已全部分配">
                         <template #default="{ row }">
-                            {{ row.boyAmount - row.boyAlreadyPlanDormitoryAmount === 0 && row.girlAmount - row.girlAlreadyPlanDormitoryAmount === 0 ? '是' : '否' }}
+                            {{ row.boyAmount - row.boyAlreadyPlanDormitoryAmount === 0 && row.girlAmount -
+                            row.girlAlreadyPlanDormitoryAmount === 0 ? '是' : '否' }}
                         </template>
                     </el-table-column>
                 </el-table>
@@ -140,13 +297,33 @@ onMounted(() => {
 
 </template>
 <style scoped>
+
+.class_info_and_plan {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+}
+.plan_form {
+    /* border: 1px solid #0483d4; */
+    padding-right: 5%;
+    .el-button{
+        color: white;
+        width: 100%;
+    }
+
+}
+
+
+
 .option_form {
     width: 100%;
     display: flex;
     /* justify-content: flex-start; */
-    
+
 
 }
+
 .form_item {
     width: 100%;
     display: flex;
@@ -158,6 +335,7 @@ onMounted(() => {
         width: auto;
         margin: 0 10px;
     }
+
     .el-select {
         width: 55%;
     }
@@ -165,6 +343,7 @@ onMounted(() => {
 
 
 }
+
 /* .el-input {
     width: 100px;
 } */
@@ -172,16 +351,18 @@ onMounted(() => {
 .el-pagination {
     margin-top: 1%;
 }
+
 .plan_domitory_container {
     background: #f0f0f0;
     height: 100vh;
-    overflow: hidden;
+    /* overflow: hidden; */
     /* padding: 1px; */
     /* display: flex; */
 }
+
 .content {
     height: 100%;
-    margin: 2% 5% 5% 5%; 
+    margin: 2% 5% 5% 5%;
     background: white;
 }
 
@@ -195,20 +376,24 @@ onMounted(() => {
 
     color: white;
     font-family: "黑体";
+
     span {
         margin-left: 5%;
         font-size: 1.3em;
     }
 }
+
 .class_table {
     margin-left: 2%;
     margin-right: 2%;
     margin-top: 2%;
     border: 1px solid #f0f0f0;
+
     span {
         color: red;
     }
 }
+
 .screen {
     height: 15%;
     /* background: red; */
@@ -219,6 +404,7 @@ onMounted(() => {
 
 
 }
+
 .option {
     display: flex;
     padding-left: 1%;
@@ -231,6 +417,7 @@ onMounted(() => {
     font-weight: 1000;
 
 }
+
 .button {
     display: flex;
     align-items: center;
@@ -239,18 +426,20 @@ onMounted(() => {
     background: #f4f4f4;
     border: 1px solid #f0f0f0;
     justify-content: flex-end;
+
     span {
         margin-right: 1%;
         color: red;
     }
 
 }
+
 /* .el-form-item {
     margin-left: 1%;
 } */
 .el-button {
     background: #0483d4;
     margin-right: 1%;
-    
+
 }
 </style>
